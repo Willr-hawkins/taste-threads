df8ee6e6a9756557210853f102539425a43fd88a
William Hawkins <Hawkinswill02@gmail.com>
2025-03-27 14:06:07 +0000

    Update build.sh file

--- build.sh
+++ build.sh
@@ -8,8 +8,16 @@ pip install --upgrade pip
 # Install requirements
 pip install -r requirements.txt
 
+# Print static files directory contents
+echo "Contents of static directory:"
+ls -la static/
+
 # Collect static files
-python manage.py collectstatic --no-input
+python manage.py collectstatic --no-input -v 2
+
+# Print staticfiles directory contents after collection
+echo "Contents of staticfiles directory:"
+ls -la staticfiles/
 
 # Run migrations
 python manage.py migrate
