e0f2d0f97376df305707e7d8ee053cc01a298dd8
William Hawkins <Hawkinswill02@gmail.com>
2025-04-04 12:58:34 +0000

    Added styling to recipe cards

--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Recipe
+from django.conf import settings
 
 def recipe_list(request):
     recipes = Recipe.objects.all().order_by('-date')  # Fetch all recipes, latest first
-    return render(request, 'recipes/recipe_list.html', {'recipes': recipes})
+    return render(request, 'recipes/recipe_list.html', {'recipes': recipes, 'MEDIA_URL': settings.MEDIA_URL})
--- tastethreads/settings.py
+++ tastethreads/settings.py
@@ -169,8 +169,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 
