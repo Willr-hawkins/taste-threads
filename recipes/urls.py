7f2db0b6669e777db7ac2575f779fcaba3147bc4
William Hawkins <Hawkinswill02@gmail.com>
2025-04-06 17:33:40 +0000

    Added link to recipes in the navbar and changed styling on recipe cards in the recipe_list.html

--- recipes/urls.py
+++ recipes/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import recipe_list
 
 urlpatterns = [
-    path('recipes/', recipe_list, name='recipe_list'),
+    path('', recipe_list, name='recipe_list'),
 ]
