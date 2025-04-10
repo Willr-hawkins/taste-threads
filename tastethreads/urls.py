6f568ffc05fe0f485bcea1d275f738901f515e71
William Hawkins <Hawkinswill02@gmail.com>
2025-04-10 17:28:58 +0000

    Attempt to fix error with serving media files via AWS S3

--- tastethreads/urls.py
+++ tastethreads/urls.py
@@ -24,4 +24,4 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
     path('recipes/', include('recipes.urls'))
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
