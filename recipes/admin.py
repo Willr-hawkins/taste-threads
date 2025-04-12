a77fbec4743bdd4664620694e49e91bbd3a9f615
William Hawkins <Hawkinswill02@gmail.com>
2025-04-12 14:21:18 +0000

    Updated recipes models so the user can list their own ingredients rather than selecting them from an exsisting list already in the database.

--- recipes/admin.py
+++ recipes/admin.py
@@ -2,12 +2,11 @@ from django.contrib import admin
 from .models import Recipe, Ingredient
 
 # Register your models here.
-class IngredientInline(admin.TabularInline):
-    model = Recipe.ingredients.through
+class IngredientInline(admin.StackedInline):
+    model = Ingredient
     extra = 1
 
 class RecipeAdmin(admin.ModelAdmin):
     inlines = [IngredientInline]
 
 admin.site.register(Recipe, RecipeAdmin)
-admin.site.register(Ingredient)
