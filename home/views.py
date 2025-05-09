30f3d08f2038de7a0be516c700053932ea28eeb7
William Hawkins <Hawkinswill02@gmail.com>
2025-05-09 14:24:01 +0000

    Added newest recipes to the home page

--- home/views.py
+++ home/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from recipes.models import Recipe
 
 def home(request):
-    return render(request, 'home/home_page.html')
+    latest_recipes = Recipe.objects.order_by('-posted_at')[:3]
+    return render(request, 'home/home_page.html', {'latest_recipes': latest_recipes})
 
